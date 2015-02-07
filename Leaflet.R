5b30a4d51fb3244deb91996a15fa75f8ebe12542
concannon <connor.concannon@gmail.com>
2015-02-07 10:42:42 -0500

    working?

--- Leaflet.R
+++ Leaflet.R
@@ -1,14 +1,14 @@
 
-install.packages(c('htmlwidgets','devtools','ggmap'))
-devtools::install_github('rstudio/leaflet')
+#install.packages(c('htmlwidgets','devtools','ggmap','rmarkdown'))
+#devtools::install_github('rstudio/leaflet')
 library(leaflet)
+library("rmarkdown")
 library(ggmap)
 
 m <- leaflet() %>% addTiles() %>% 
   setView(-73.974188,40.732358,zoom=11 ) %>%
   addPopups(-73.911,40.762,'Home!') %>%
-  addPopups(-73.987,40.770,'Jack"s BirthPlace')
+  addPopups(-73.987,40.770,'Jack"s BirthPlace') %>%
+  addPopups(-74.00,40.71,'Work')
 
 m
-
-getwd()
